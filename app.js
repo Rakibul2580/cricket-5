c52c99c709c5954a53ab815efec4b7e365339bab
Rakibul2580 <rh565153@gmail.com>
2022-08-22 12:55:05 +0300

    html com

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ function teamMembersName(btn){
     const name = btn.parentNode.children[0].innerText
     membersName.push(name);
     teamMembersList()
+    btn.disabled = 'true'
     console.log(membersName)
 }
 
@@ -10,7 +11,6 @@ function teamMembersList(){
     if(membersName.length <= 5){
         const teamMembers = document.getElementById('members')
         const li = document.createElement('li')
-        // li.innerText = ''
         for(let i = 0; i < membersName.length; i++){
             li.innerText = `
             ${membersName[i]}
@@ -21,3 +21,36 @@ function teamMembersList(){
         alert('Full')
     }
 }
+
+function stringValue(elementId){
+    const elementValue = document.getElementById(elementId)
+    const elementValueString = elementValue.value
+    const elementValueNumber = parseFloat(elementValueString)
+    return elementValueNumber;
+}
+document.getElementById('calculate').addEventListener('click', function(){
+    const player = stringValue('per-player')
+    const players = player * membersName.length
+
+    const expenses = document.getElementById('expenses')
+    expenses.innerText = players
+    player.value = ''
+})
+
+document.getElementById('total-calculate').addEventListener('click', function(){
+    const expensess = stringValue('expenses')
+    const expensesString = expensess.innerText
+    const expensesNumber = parent(expensesString)
+console.log(expensesNumber)
+    // const manager = stringValue('manager')
+    // const coach = stringValue('coach')
+
+    // const totalExpenses = expensesNumber + manager + coach
+
+    // const total = document.getElementById('total')
+    // total.innerText = totalExpenses
+    // expenses.value = ''
+    // manager.value = ''
+    // coach.value = ''
+
+})
